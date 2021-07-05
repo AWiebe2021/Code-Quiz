265234c88f7bb29972f4a93b8c05f5b2052b47a5
Aaron Wiebe <awiebe1@cox.net>
2021-07-05 14:07:11 -0700

    basic data and flow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,175 @@
-// The array of questions for the game.
-var exampleQuestions = [
-  { q: 'The sky is blue.', a: 't' },
-  { q: 'There are 365 days in a year.', a: 't' },
-  { q: 'There are 42 ounces in a pound.', a: 'f' },
-  { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-  { q: 'Bananas are vegetables.', a: 'f' }
-];
-questions = new Array();
-questions = [
-  'Which type of JavaScript language is ___.',
-  'Which of the following is the correct output for the following JavaScript code: \
-  varx=5,y=1 \
-  var obj ={ x:10} \
-  with(obj) \
-  { \
-    alert(y)\
-  }',
-  'Which one of the following also known as Conditional Expression:',
-  'Among the following given JavaScript snipped codes, which is more efficient: \
-  Code A \
-  for(var number=10;number>=1;number--)  \
-  { \
-    document.writeln(number);  \
-  } \
-  Code B \
-  var number=10;  \
-  while(number>=1)  \
-  {  \
-    document.writeln(number);  \
-    number++;  \
-  }',
-  'In JavaScript, what is a block of statement?'
-];
+// We start the game with a score of 0.
+var score = 0;
+var questionIndex=0;
+// var questionState = 'unanswered';
+var timerEl = document.getElementById('countdown');
+var scoreEl = document.getElementById('score');
+var timeLeft = 30;
+var outOfTime = false;
+var timeInterval;
 
-answersArray= new Array();
-answersArray[0] = [
-  {a: 'Object-Oriented', c: 'Wrong'},
-   {a: 'Object-Based', c: 'Correct'},
-   {a: 'Assembly-language', c: 'Wrong'},
-   {a: 'High-level', c: 'Wrong'}
-];
-answersArray[1] = [
-  {a: '1', c: 'Correct'},
-   {a: 'Error', c: 'Wrong'},
-   {a: '10', c: 'Wrong'},
-   {a: '5', c: 'Wrong'}
-];
-answersArray[2] = [
-  {a: 'Alternative to if-else', c: 'Wrong'},
-   {a: 'Switch statement', c: 'Wrong'},
-   {a: 'If-then-else statement', c: 'Wrong'},
-   {a: 'immediate if', c: 'Correct'}
-];
-answersArray[3] = [
-  {a: 'Code 1', c: 'Correct'},
-   {a: 'Code 2', c: 'Wrong'},
-   {a: 'Both Code 1 and Code 2', c: 'Wrong'},
-   {a: 'Cannot Compare', c: 'Wrong'}
-];
-answersArray[4] = [
-  {a: 'Conditional block', c: 'Wrong'},
-   {a: 'block that combines a number of statements into a single compound statement', c: 'Correct'},
-   {a: 'both conditional block and a single statement', c: 'Wrong'},
-   {a: 'block that contains a single statement', c: 'Wrong'}
-];
-// console.log("Hello world!");
-// console.log(answersArray[0][0].c);
-// console.table(answersArray);
+// timerEl.textContent = "";
+// scoreEl.textContent  =  "";
+var questionEl = document.getElementById("question");
+questionEl.innerText = 'Let us Begin';
+var answersEl = document.getElementById("answers");
+while (answersEl.hasChildNodes()) {  
+  answersEl.removeChild(answersEl.firstChild);
+};
 
-// // We start the game with a score of 0.
-  var score = 0;
+var listItemEl = document.createElement("li");
+listItemEl.appendChild(document.createTextNode(""));
+var button = document.createElement("button");
+button.className = "answer-button";
+button.innerHTML = ("Start Quiz");
+button.onclick = startGame;
+listItemEl.appendChild(button);
+answersEl.appendChild(listItemEl);
+
+function startGame(){
+  // The array of questions for the game.
+  questionsArray = new Array();
+  questionsArray = [
+    'Which type of JavaScript language is ___.',
+    'Which of the following is the correct output for the following JavaScript code: \n var x=5,y=1 \n var obj ={ x:10} \n with(obj) \n { \n  alert(y) \n }',
+    'Which one of the following also known as Conditional Expression:',
+    'Among the following given JavaScript snipped codes, which is more efficient: \n Code A \n for(var number=10;number>=1;number--) \n  { \n    document.writeln(number);  \n  } \n  Code B \n  var number=10;  \n  while(number>=1)  \n  {  \n    document.writeln(number);  \n    number++;  \n  }',
+    'In JavaScript, what is a block of statement?'
+  ];
+  // The array of answers for the game.
+  answersArray= new Array();
+  answersArray[0] = [
+    {a: 'Object-Oriented', c: 'Wrong'},
+    {a: 'Object-Based', c: 'Correct'},
+    {a: 'Assembly-language', c: 'Wrong'},
+    {a: 'High-level', c: 'Wrong'}
+  ];
+  answersArray[1] = [
+    {a: '1', c: 'Correct'},
+    {a: 'Error', c: 'Wrong'},
+    {a: '10', c: 'Wrong'},
+    {a: '5', c: 'Wrong'}
+  ];
+  answersArray[2] = [
+    {a: 'Alternative to if-else', c: 'Wrong'},
+    {a: 'Switch statement', c: 'Wrong'},
+    {a: 'If-then-else statement', c: 'Wrong'},
+    {a: 'immediate if', c: 'Correct'}
+  ];
+  answersArray[3] = [
+    {a: 'Code 1', c: 'Correct'},
+    {a: 'Code 2', c: 'Wrong'},
+    {a: 'Both Code 1 and Code 2', c: 'Wrong'},
+    {a: 'Cannot Compare', c: 'Wrong'}
+  ];
+  answersArray[4] = [
+    {a: 'Conditional block', c: 'Wrong'},
+    {a: 'block that combines a number of statements into a single compound statement', c: 'Correct'},
+    {a: 'both conditional block and a single statement', c: 'Wrong'},
+    {a: 'block that contains a single statement', c: 'Wrong'}
+  ];
+  createQuestion();
+  // // reset conditions
+  // questionState = 'unanswered';
+  // outOfTime = false;
+  // timeLeft = 30;
+  // clearInterval(timeInterval);
+  // // Start Timer
+  // countdown();
+};
+
+function createQuestion(){
+  var questionEl = document.getElementById("question");
+  questionEl.innerText = questionsArray[questionIndex];
+  // clear former answers
+  var answersEl = document.getElementById("answers");
+  while (answersEl.hasChildNodes()) {  
+    answersEl.removeChild(answersEl.firstChild);
+  };
+  //  Create new answer buttons
+  for (var answerIndex = 0; answerIndex < answersArray[questionIndex].length; answerIndex++) {
+    createAnswers(answerIndex);
+  };
+  // reset conditions
+  // questionState = 'unanswered';
+  outOfTime = false;
+  timeLeft = 30;
+  clearInterval(timeInterval);
   
-  // // Loop over every question object
-  for (var i = 0; i < questions.length; i++) {
-    var aWrapperEl = document.getElementsByClassName("answers-wrapper");
-    var questionEl = document.getElementById("question");
-      questionEl.innerText = questions[i];
-      questionEl.append;
-    //clear former answers
-    var answersEl = document.getElementById("answers");
-    while (answersEl.hasChildNodes()) {  
-      answersEl.removeChild(answersEl.firstChild);
-    }
-    answersArray[i].forEach(createAnswers);
-      /// start a timer to answer
+  // Start Timer
+  countdown();
+};
 
-  //   // Compare answers
-  //   if (
-  //     (answer === true && questions[i].a === 't') ||
-  //     (answer === false && questions[i].a === 'f')
-  //   ) {
-  //     // Increase score
-  //     score++;
-  //     // Alert the user
-  //     alert('Correct!');
-  //   } else {
-  //     alert('Wrong!');
-  //   }
+
+function createAnswers(answerIndex){
+  var answersEl = document.getElementById("answers");
+  var listItemEl = document.createElement("li");
+  listItemEl.appendChild(document.createTextNode(""));
+  var button = document.createElement("button");
+  button.className = "answer-button";
+  button.innerHTML = (answersArray[questionIndex][answerIndex].a);
+  button.setAttribute("conclusion",(answersArray[questionIndex][answerIndex].c));
+  button.onclick = scoreAnswer;
+  listItemEl.appendChild(button);
+  answersEl.appendChild(listItemEl);
+};
+
+// Timer that counts down from 30
+function countdown() {
+  outOfTime = false;    
+  timeInterval = setInterval(function() {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+       timerEl.textContent = timeLeft;
+       clearInterval(timeInterval);
+       outOfTime = true;
+      //  questionState = 'answered';
+       scoreAnswer();
+    };
+  }, 1000);
+};
+
+function scoreAnswer(evt){
+  if (outOfTime){
+    timerEl.textContent = 'OOT';
+    // alert('You are out of time')
+  }else{
+    // if (questionState === 'unanswered'){
+      conclusion = evt.currentTarget.getAttribute("conclusion")
+      if((conclusion === "Correct")){
+        score++;
+      };
+      // alert('You got it ' + conclusion);
+      // questionState = 'answered';
+    // }else{
+    //   // alert('This Question has been answered');
+    // };
+  };
+  scoreEl.textContent  =  "Your Score: " + score + "/" + (questionIndex);
+  if (questionIndex < questionsArray.length){
+    createQuestion();
+    questionIndex++;
+  }else{
+    endGame();
   }
-  
-  function createAnswers(item,i2){
-    var answersEl = document.getElementById("answers");
-    var listItemEl = document.createElement("li");
-    listItemEl.appendChild(document.createTextNode(""));
+};     
 
-    var button = document.createElement("button");
-    button.className = "answer-button";
-    button.innerHTML = (answersArray[i][i2].a);
-    listItemEl.appendChild(button);
-    listItemEl.setAttribute("conclusion",(answersArray[i][i2].c));
-    answersEl.appendChild(listItemEl);
-
-    // listItemEl.textContent = (answersArray[i][i2].a);
-    // answersEl.appendChild(listItemEl);
-  }
-
-   
-  // // Show total at end
-  // alert('You got ' + score + '/' + questions.length);
-
-
-  // var buttonEl = document.querySelector("#save-task");
-  // var answersEl = document.querySelector("#answers");
-  
-  // var createTaskHandler = function() {
-  //   var listItemEl = document.createElement("li");
-  //   listItemEl.className = "answer-button";
-  //   listItemEl.textContent = "This is a new task.";
-  //   tasksToDoEl.appendChild(listItemEl);
-  // };
-  
-  // buttonEl.addEventListener("click", createTaskHandler);
-    
+function endGame(){
+  timerEl.textContent = "";
+  scoreEl.textContent  =  "";
+  var questionEl = document.getElementById("question");
+  questionEl.innerText = 'All Done';
+  var answersEl = document.getElementById("answers");
+  while (answersEl.hasChildNodes()) {  
+    answersEl.removeChild(answersEl.firstChild);
+  };
+  var listItemEl = document.createElement("li");
+  listItemEl.appendChild(document.createTextNode("Your Score: " + score + "/" + (questionIndex)));
+  answersEl.appendChild(listItemEl);
+  clearInterval(timeInterval);
+};
